98e8668dcc53dfcd2389a8dc47482f8cd9a7eaa0
wujianqinjian <wujianqinjian@163.com>
2021-04-13 10:14:31 +0800

    项目初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,9 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>This is test！</div>
   </div>
+
 </template>
 
 <script>
