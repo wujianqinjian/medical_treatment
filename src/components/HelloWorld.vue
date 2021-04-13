9e95dbfeb374df8259342fc378df201aa00be47d
wujianqinjian <wujianqinjian@163.com>
2021-04-13 10:25:34 +0800

    医疗测试项目初始化！

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-    <div>用来练习测试技能的前端项目！</div>
+    <div>练习测试技能前端！</div>
   </div>
 
 </template>
