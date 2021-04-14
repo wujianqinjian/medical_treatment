864dc0ce85883e108d14a4247f71ae9c999f1e2c
wujianqinjian <wujianqinjian@163.com>
2021-04-14 23:28:40 +0800

    添加基本配置项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import ElementUI from 'element-ui';
+import {Button,Select} from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+//Vue.use(ElementUI);
+
+Vue.component('button.ui',Button)
+Vue.component('Select.ui',Select)
+
 
 new Vue({
   render: h => h(App),
