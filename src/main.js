5f72a94474b23e2f7f461db41b03bb5859ea3dd7
wujianqinjian <wujianqinjian@163.com>
2021-04-14 23:30:21 +0800

    添加基本配置项目

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 //Vue.use(ElementUI);
+//test
 
 Vue.component('button.ui',Button)
 Vue.component('Select.ui',Select)
